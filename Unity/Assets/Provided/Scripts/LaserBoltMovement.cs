df6ae9af0042c32b8af347e38e9cfabe244c6b3c
Matt Van Der Westhuizen <matt@chaotik.co.za>
2016-08-10 22:53:24 +0200

    Initial commit.

---
+++ Unity/Assets/Provided/Scripts/LaserBoltMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBoltMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    Rigidbody2D body;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+
+        body.AddForce(new Vector2(0, speed));
+    }
+
+    void Update() {
+        if (Input.GetButton("Fire1")) {
+
+        }
+    }
+}
---
+++ Unity/Assets/Provided/Scripts/PlayerShootLasers.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootLasers : MonoBehaviour {
+
+    public Transform shot;
+    public float energyCostPerShot = 10.0f;
+    public float energyPerSecond = 20.0f;
+
+    private Transform leftGun;
+    private Transform rightGun;
+    private float energy = 0.0f;
+
+	void Start () {
+        leftGun = transform.FindChild("LeftGun");
+        rightGun = transform.FindChild("RightGun");
+        energy = energyCostPerShot;
+	}
+	
+	void Update () {
+	    if (energy < energyCostPerShot) {
+            energy += Time.deltaTime * energyPerSecond;
+        }
+
+        if (Input.GetButton("Fire1")) {
+            energy -= energyCostPerShot;
+
+            Instantiate(shot, leftGun);
+            Instantiate(shot, rightGun);
+        }
+	}
+}
