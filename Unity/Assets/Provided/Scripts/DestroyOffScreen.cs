18e7106284fdb4ca41de723824560b41fd56cd73
Matt Van Der Westhuizen <matt@chaotik.co.za>
2016-08-12 18:44:19 +0200

    Basic collision handling. Delete shots that are no longer visible.

---
+++ Unity/Assets/Provided/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOffScreen : MonoBehaviour {
+
+    private Renderer objectRenderer;
+
+	// Use this for initialization
+	void Start () {
+        objectRenderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!objectRenderer.isVisible) {
+            Destroy(gameObject);
+        }
+	}
+}
